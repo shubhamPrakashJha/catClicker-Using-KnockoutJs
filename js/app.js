6074bd37e81dde2def9186a248374850ccd71385
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-07-28 15:23:16 +0530

    Seperated out the model.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,9 @@
-var ViewModel = function () {
+var Cat = function () {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.nicknames = ko.observableArray(["Tabtab","T-boe","Mr. T","Catty"]);
     // this.imgAttribution = ko.observable("")
-
-    this.incrementCounter = function () {
-        this.clickCount(this.clickCount() +1);
-    };
 
     this.levels = ko.computed(function () {
         if(this.clickCount() < 3){
@@ -17,10 +13,21 @@ var ViewModel = function () {
             return "Infant";
         }else if(this.clickCount() <20){
             return "Teen";
-        }else {
+        }else if(this.clickCount() <30){
             return "Adult";
+        } else {
+            return "Ninja";
         }
     }, this)
+};
+
+var ViewModel = function () {
+
+    this.currentCat = ko.observable(new Cat());
+
+    this.incrementCounter = function () {
+        this.currentCat().clickCount(this.currentCat().clickCount() +1);
+    };
 };
 
 ko.applyBindings(new ViewModel());
