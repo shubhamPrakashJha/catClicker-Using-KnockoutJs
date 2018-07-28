6ba8c481c815eaa201337a7cff46d82e70b21290
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-07-28 13:52:08 +0530

    Model For Cat Clicker using Knockout

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 var ViewModel = function () {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    // this.imgAttribution = ko.observable("")
 
 };
 
