10e3c4c6d9c8ab8c1bf26dfc8dec2bc5d467531e
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-07-28 15:03:55 +0530

    Add List of nicknames to cat using observableArray.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var ViewModel = function () {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.nicknames = ko.observableArray(["Tabtab","T-boe","Mr. T","Catty"]);
     // this.imgAttribution = ko.observable("")
 
     this.incrementCounter = function () {
